c1b5392bf453f26c07545f636fb16758be7690fe
bankbank123 <chidchanun.bank@mail.kmutt.ac.th>
2026-06-27 00:40:17 +0700

    - update allow ip

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["192.168.1.50", "chidchanun.online"],
 };
 
 export default nextConfig;
